cb1d6a6609de77005510a1800f97887561fd5cae
KOEUN SEO <egs125@gmail.com>
2019-02-13 17:30:02 +0900

    Create MyNode.java

---
+++ MyNode.java
@@ -0,0 +1,24 @@
+package javaStudy;
+
+/**
+ * @Class        : javaStudy/MyNodeList.java
+ * @Description  : 
+ * @author       : egs125
+ * @since        : 2019. 2. 12.
+ * @version      : 1.0
+ * @see
+ */
+
+public class MyNode {
+
+	private int val;
+
+	public int getVal() {
+		return val;
+	}
+	public void setVal(int val) {
+		this.val = val;
+	}
+	
+	
+}
